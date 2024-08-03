01d629a1a66ba0ac5bf9a71b0a23427f2c5afbff
CappyGerald <geraldodhiambo67@gmail.com>
2024-08-03 16:06:07 +0300

    Registered the Employee model

--- project_manager/admin.py
+++ project_manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Activity, Project, Personel, Machinery, Material, Milestone
+from .models import Activity,  Employee, Project, Personel, Machinery, Material, Milestone
 # Register your models here.
 admin.site.register(Activity)
 admin.site.register(Project)
@@ -7,3 +7,4 @@ admin.site.register(Personel)
 admin.site.register(Machinery)
 admin.site.register(Material)
 admin.site.register(Milestone)
+admin.site.register(Employee)
