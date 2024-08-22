bdc2f149801b49e6adfd4ea45873c30f4c53eb86
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2024-08-22 20:46:51 +0300

    deregistered the Personnel Model

--- project_manager/admin.py
+++ project_manager/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from .models import Activity,  Employee, Project, Personnel, Machinery, Material, Milestone, Status
+from .models import Activity,  Employee, Project,  Machinery, Material, Milestone
 # Register your models here.
 admin.site.register(Activity)
 admin.site.register(Project)
-admin.site.register(Personnel)
 admin.site.register(Machinery)
 admin.site.register(Material)
 admin.site.register(Milestone)
 admin.site.register(Employee)
-admin.site.register(Status)
