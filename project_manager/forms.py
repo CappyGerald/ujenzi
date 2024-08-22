36fc2aacc1a96771903ef515375bc39811edfd39
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2024-08-22 20:45:51 +0300

    deleted PersonnelForm added join_date, end_date and project fields to EmloyeeForm

--- project_manager/forms.py
+++ project_manager/forms.py
@@ -1,6 +1,6 @@
 
 from django.forms import ModelForm
-from .models import Activity,Employee, Project, Personnel,Machinery, Material, Milestone
+from .models import Activity,Employee, Project,Machinery, Material, Milestone
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
@@ -14,7 +14,7 @@ class ProjectForm(ModelForm):
 
     class Meta:
         model = Project
-        fields = ['name', 'description', 'budget', 'start_date', 'end_date', 'expected_revenue', 'revenue_unit']
+        fields = ['name', 'description', 'budget', 'start_date', 'end_date', 'expected_revenue', 'revenue_unit', 'status']
 
 class MachineryForm(ModelForm):
     def __init__(self, *args, **kwargs):
@@ -35,16 +35,6 @@ class MaterialForm(ModelForm):
     class Meta:
         model = Material
         fields = ['project','name', 'quantity', 'quantity_units', 'cost', 'cost_unit' ]
-
-class PersonnelForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method ='post'
-        self.helper.add_input(Submit('submit', 'save'))
-    class Meta:
-        model = Personnel
-        fields = ['project','employee','join_date', 'end_date']
 
 class ActivityForm(ModelForm):
     def __init__(self, *args, **kwargs):
@@ -74,5 +64,5 @@ class EmployeeForm(ModelForm):
         self.helper.add_input(Submit('submit', 'save'))
     class Meta:
         model = Employee
-        fields = ['first_name', 'last_name', 'email', 'department', 'position']
+        fields = ['project','first_name', 'last_name', 'email','join_date' , 'end_date', 'department', 'position']
 
